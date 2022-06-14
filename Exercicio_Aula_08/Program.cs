423272f4fac588a565c757f9e2a1f0d777d695ff
Sabrina Helena <sabrinahelenaf@gmail.com>
2022-06-14 20:23:58 -0300

    adicionei um menu de opções para o usuário criado

--- Exercicio_Aula_08/Program.cs
+++ Exercicio_Aula_08/Program.cs
@@ -59,8 +59,41 @@
         static void Main(string[] args)
         {
             MenuFilmes usuario1 = new MenuFilmes();
- 
+            Console.WriteLine($"Menu de opções: \r\n" +
+                $"1 - Apresenta todos os filme separados por ; (ponto e vírgula) \r\n" +
+                $"2 - Apresenta quantidade de filmes na lista \r\n" +
+                $"3 - Busca o nome do filme de acordo com o seu índice \r\n" +
+                $"4 - Adiciona um novo filme a lista pelo seu nome \r\n" +
+                $"5 - Atualiza um filme a partir do seu índice na lista \r\n" +
+                $"6 - Lista todos os filmes com o seu índice/posição na lista");
+            int opcao = Convert.ToInt32(Console.ReadLine());
 
+            switch (opcao)
+            {
+                case 1:
+                    usuario1.ApresentarTodosOsFilmesSeparadosPorPontoVirgula();
+                    break;
+                case 2:
+                    usuario1.ExibirQuantidadeTotalDeFilmes();
+                    break;
+                case 3:
+                    usuario1.buscarFilmePeloIndice();
+                    break;
+                case 5:
+                    usuario1.atualizarUmFilmePeloSeuIndice();
+                    break;
+                case 6:
+                    usuario1.listarTodosOsFilmesComSeuIndiceNaLista();
+                    break;
+                default:
+                    Console.WriteLine("Digite uma opção válida");
+                    break;
+
+            }
+            /*basta digitar usuario1. e escolher o método que quer utilizar. 
+             * criei como usuario1, pois a lógica é o usuario utilizar o programa
+             * para usar os métodos para si, cada um que for utilizar pode criar um novo usuário.
+             */
         }
     }
 }
