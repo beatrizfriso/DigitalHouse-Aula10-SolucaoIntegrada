31b85e26abfa7ff89d26677fe433b31760cf3aa5
cassiofreitas <cassiofreitas@cassiofreitas.com>
2022-06-14 22:51:20 -0300

    Versão inicial, já contendo implementação simples da atividade da aula 6 (calcular ano de nascimento).

--- Aula10_solucao_integrada/Program.cs
+++ Aula10_solucao_integrada/Program.cs
@@ -2,9 +2,39 @@
 {
     internal class Program
     {
+        public class Calculadora
+        {
+            //calcular ano de nascimento
+            //converter dolares
+            //fazer operacoes basicas
+            public string NomeDoUsuario = string.Empty;
+            public int IdadeDoUsuario;
+            private int AnoAtual = 2022;
+            public int AnoDeNascimento;
+            public int CalculaAnoNascimento()
+            {
+                return this.AnoDeNascimento = this.AnoAtual - IdadeDoUsuario;
+            }
+        }
+        public class Filmes
+        {
+            //fazer movimentacoes dos filmes
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Calculadora calculadora = new();
+            Console.Beep();
+            Console.Clear();
+
+            Console.Write("Para o calculo do ano de nascimento, qual seu nome? > ");
+            calculadora.NomeDoUsuario = (Convert.ToString(Console.ReadLine()));
+            Console.Write("Para o calculo do ano de nascimento, qual sua idade atual? > ");
+            calculadora.IdadeDoUsuario = (Convert.ToInt16(Console.ReadLine()));
+            Console.WriteLine($"Ano de nascimento = {calculadora.CalculaAnoNascimento()}");
+
+
         }
     }
 }
